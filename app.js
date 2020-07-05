3a2c29eccffec83aa4f2f9964be62619de2bc328
JMan <60026263+JMantis0@users.noreply.github.com>
2020-07-04 20:36:07 -0700

    Added option under view to display employees under a specific manager

--- app.js
+++ app.js
@@ -41,40 +41,67 @@ figlet('Employee', function(err, data) {
 // Function viewData takes uses data parameter in a switch to the proper query 
 async function viewData(data) {
 	let query;
-	if (data === 'Employees by Manager') {
-		console.log('Not Available Yet');
-    await main();
-
-	}
 	switch(data) {
 		case('Departments'):
-		query = `SELECT id AS ID, name AS DEPARTMENT FROM department`;
-		console.log("--------------------------------------------------------------------");
-		break;
+      query = `SELECT id AS ID, name AS DEPARTMENT FROM department`;
+      console.log("--------------------------------------------------------------------");
+      break;
 		case('Employees'):
-		query = "SELECT t1.id AS ID, CONCAT(t1.first_name, ' ', t1.last_name) AS NAME, t3.title AS TITLE, t4.name AS DEPARTMENT, t3.salary AS SALARY, CONCAT(t2.first_name, ' ', t2.last_name) as MANAGER FROM employee t1 LEFT JOIN employee t2 ON t1.manager_id = t2.id LEFT JOIN role t3 ON t1.role_id = t3.id LEFT JOIN department t4 ON t3.department_id = t4.id ORDER BY t1.id";
-		console.log("--------------------------------------------------------------------\nEMPLOYEES\n--------------------------------------------------------------------");
-		break;
+      query = "SELECT t1.id AS ID, CONCAT(t1.first_name, ' ', t1.last_name) AS NAME, t3.title AS TITLE, t4.name AS DEPARTMENT, t3.salary AS SALARY, CONCAT(t2.first_name, ' ', t2.last_name) as MANAGER FROM employee t1 LEFT JOIN employee t2 ON t1.manager_id = t2.id LEFT JOIN role t3 ON t1.role_id = t3.id LEFT JOIN department t4 ON t3.department_id = t4.id ORDER BY t1.id";
+      console.log("--------------------------------------------------------------------\nEMPLOYEES\n--------------------------------------------------------------------");
+      break;
+    case('Employees by Manager'):
+     query = await viewByManager();
+     console.log("--------------------------------------------------------------------\nBY MANAGER\n--------------------------------------------------------------------");
+     break;
 		case('Roles'):
-		query = `SELECT DISTINCT t1.id AS ID, title AS TITLE, t3.name AS DEPARTMENT, salary AS SALARY FROM role t1 LEFT JOIN employee t2 ON t1.id = t2.role_id LEFT JOIN department t3 ON t1.department_id = t3.id`;
-		console.log("--------------------------------------------------------------------\nROLES\n--------------------------------------------------------------------");
-		break;
+      query = `SELECT DISTINCT t1.id AS ID, title AS TITLE, t3.name AS DEPARTMENT, salary AS SALARY FROM role t1 LEFT JOIN employee t2 ON t1.id = t2.role_id LEFT JOIN department t3 ON t1.department_id = t3.id`;
+      console.log("--------------------------------------------------------------------\nROLES\n--------------------------------------------------------------------");
+      break;
 		
 	}
 
 	connection.query(query, function(err, result) {
     if (err) throw err;
-    console.log("query ********")
 		console.table(result);
 		main()
 	});	
 }
 
-// Function is to dipsplay employees by manager afte ruser chooses the manager
-function viewByManager() {
-  let managerData = queryManagers();
-  console.log(managerData)
+// Function is to display employees by manager after user chooses the manager
+async function viewByManager() {
+  let managerData = await queryManagers();
+  //Rearrange managerData into object for easier use in inquirer/mysql
+  let list = {};
+  for (packet of managerData) {
+    list[packet.id] = {
+      id: packet.id,
+      name: packet.name,
+      title: packet.title,
+      managerID: packet.manager_id
+    }
+  }
 
+  let {manager} = await inq.prompt([
+    {
+      type: 'list',
+      name: 'manager',
+      message: 'View employees managed by: ',
+      choices: function() {
+        return Object.keys(list).map(key => list[key].name);
+      }
+    },
+  ]);
+
+  //Find id of manager
+  let managerID;
+  for(key of Object.keys(list)) {
+    if(list[key].name === manager) {
+      managerID = list[key].id;
+      break
+    }
+  }
+  return `SELECT t2.id AS ID, concat(t2.first_name, ' ', t2.last_name) as EMPLOYEE, t3.title AS TITLE, concat(t1.first_name, ' ', t1.last_name) as 'MANAGED BY' FROM employee t1 LEFT JOIN employee t2 ON t1.id = t2.manager_id LEFT JOIN role t3 ON t1.role_id = t3.id WHERE t2.manager_id = ${managerID}`;
 }
 
 //  Function addData uses data param in a switch to call the proper add function
@@ -424,14 +451,12 @@ async function main() {
 	];
 
 	let {action, data} = await inq.prompt(mPrompt);
-  console.log("data: ***" ,data);
 	if(data === "Yes") {
 		console.log("Goodbye!");
 		connection.end();
 		process.exit();
 	}
 	else if(data === "No") {
-    console.log("Line 432");
 		main();
 	}
 	else {
@@ -470,6 +495,6 @@ async function queryEmployee() {
 
 async function queryManagers() {
 
-	return connection.query("SELECT id, concat(first_name, ' ', last_name) as name, concat(id, ' ', first_name, ' ', last_name) as manager from employee WHERE manager_id IS NULL");
+	return connection.query("SELECT t1.id, concat(t1.first_name, ' ', t1.last_name) as name, t2.title, t1.manager_id, concat(t1.id, ' ', t1.first_name, ' ', t1.last_name) as manager from employee t1 LEFT JOIN role t2 ON t2.id = t1.role_id  WHERE manager_id IS NULL");
 
 }
